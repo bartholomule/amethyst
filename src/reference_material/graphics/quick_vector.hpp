16becf268dd3c4a529c4f5d3662b88e7cea9652f
kpharris <kpharris>
2004-04-12 02:58:21 +0000

    Listing functions as inline.  Changing the pointer-based constructor of the iterators to be explicit.  Greatly improving performance by preallocating space in empty vectors, and using the next power of two for the storage size when copying an existing vector or resizing an existing vector.

--- src/reference_material/graphics/quick_vector.hpp
+++ src/reference_material/graphics/quick_vector.hpp
@@ -1,5 +1,5 @@
 /*
- * $Id: quick_vector.hpp,v 1.3 2004/04/11 13:43:16 zorthluthik Exp $
+ * $Id: quick_vector.hpp,v 1.4 2004/04/12 02:58:21 kpharris Exp $
  *
  * Part of "Amethyst" -- A playground for graphics development.
  * Copyright (C) 2004 Kevin Harris
@@ -47,69 +47,69 @@ namespace amethyst
   class simple_iterator : public std::iterator<std::random_access_iterator_tag,T,size_t,T*,T&>
   {
   public: 
-    simple_iterator(): ptr(NULL)
+    inline simple_iterator(): ptr(NULL)
     {
     }
-    simple_iterator(T* p): ptr(p)
+    inline explicit simple_iterator(T* p): ptr(p)
     {
     }
-    simple_iterator(const simple_iterator<T>& old): ptr(old.ptr)
+    inline simple_iterator(const simple_iterator<T>& old): ptr(old.ptr)
     {
     }
-    ~simple_iterator()
+    inline ~simple_iterator()
     {
     }
-    simple_iterator& operator=(const simple_iterator<T>& old)
+    inline simple_iterator& operator=(const simple_iterator<T>& old)
     {
       ptr = old.ptr;
       return *this;
     }
     
-    T* operator->() { return ptr; }
-    T& operator*() { return *ptr; }
-    simple_iterator operator++() 
+    inline T* operator->() { return ptr; }
+    inline T& operator*() { return *ptr; }
+    inline simple_iterator operator++() 
     {
       ++ptr;
       return *this;
     }
-    simple_iterator operator++(int) 
+    inline simple_iterator operator++(int) 
     {
       simple_iterator temp(*this);
       ++ptr;
       return temp;
     }
-    bool operator!=(const simple_iterator& it) const
+    inline bool operator!=(const simple_iterator& it) const
     {
       return ptr != it.ptr;
     }
-    bool operator==(const simple_iterator& it) const
+    inline bool operator==(const simple_iterator& it) const
     {
       return ptr == it.ptr;
     }
-    bool operator<(const simple_iterator& it) const
+    inline bool operator<(const simple_iterator& it) const
     {
       return ptr < it.ptr;
     }
-    simple_iterator<T> operator--() 
+    inline simple_iterator<T> operator--() 
     {
       --ptr;
       return *this;
     }
-    simple_iterator<T> operator--(int) 
+    inline simple_iterator<T> operator--(int) 
     {
       simple_iterator<T> temp(*this);
       --ptr;
       return temp;
     }
-    size_t operator-(const simple_iterator<T>& iter) const
+    inline size_t operator-(const simple_iterator<T>& iter) const
     {
       return ptr - iter.ptr;
     }
-    simple_iterator<T> operator+(size_t i) const
+    inline simple_iterator<T> operator+(size_t i) const
     {
       return simple_iterator<T>(ptr + i);
     }
-    simple_iterator<T> operator-(size_t i) const
+    inline simple_iterator<T> operator-(size_t i) const
     {
       return simple_iterator<T>(ptr - i);
     }
@@ -124,69 +124,69 @@ namespace amethyst
   class const_simple_iterator : public std::iterator<std::random_access_iterator_tag,const T,size_t,const T*,const T&>
   {
   public:
-    const_simple_iterator(): ptr(NULL)
+    inline const_simple_iterator(): ptr(NULL)
     {
     }
-    explicit const_simple_iterator(const T* p): ptr(p)
+    inline explicit const_simple_iterator(const T* p): ptr(p)
     {
     }
-    const_simple_iterator(const const_simple_iterator<T>& old): ptr(old.ptr)
+    inline const_simple_iterator(const const_simple_iterator<T>& old): ptr(old.ptr)
     {
     }
-    ~const_simple_iterator()
+    inline ~const_simple_iterator()
     {
     }
-    const_simple_iterator& operator=(const const_simple_iterator<T>& old)
+    inline const_simple_iterator& operator=(const const_simple_iterator<T>& old)
     {
       ptr = old.ptr;
       return *this;
     }
     
-    const T* operator->() const { return ptr; }
-    const T& operator*() const { return *ptr; }
-    const_simple_iterator operator++() 
+    inline const T* operator->() const { return ptr; }
+    inline const T& operator*() const { return *ptr; }
+    inline const_simple_iterator operator++() 
     {
       ++ptr;
       return *this;
     }
-    const_simple_iterator operator++(int) 
+    inline const_simple_iterator operator++(int) 
     {
       const_simple_iterator temp(*this);
       ++ptr;
       return temp;
     }
-    bool operator!=(const const_simple_iterator& it)
+    inline bool operator!=(const const_simple_iterator& it)
     {
       return ptr != it.ptr;
     }
-    bool operator==(const const_simple_iterator& it)
+    inline bool operator==(const const_simple_iterator& it)
     {
       return ptr == it.ptr;
     }
-    bool operator<(const const_simple_iterator& it) const
+    inline bool operator<(const const_simple_iterator& it) const
     {
       return ptr < it.ptr;
     }    
-    const_simple_iterator<T> operator--() 
+    inline const_simple_iterator<T> operator--() 
     {
       --ptr;
       return *this;
     }
-    const_simple_iterator<T> operator--(int) 
+    inline const_simple_iterator<T> operator--(int) 
     {
       const_simple_iterator<T> temp(*this);
       --ptr;
       return temp;
     }
-    size_t operator-(const const_simple_iterator<T>& iter) const
+    inline size_t operator-(const const_simple_iterator<T>& iter) const
     {
       return ptr - iter.ptr;
     }
-    const_simple_iterator<T> operator+(size_t i) const
+    inline const_simple_iterator<T> operator+(size_t i) const
     {
       return const_simple_iterator<T>(ptr + i);
     }
-    const_simple_iterator<T> operator-(size_t i) const
+    inline const_simple_iterator<T> operator-(size_t i) const
     {
       return const_simple_iterator<T>(ptr - i);
     }    
@@ -212,7 +212,7 @@ namespace amethyst
    * be gained by NOT defining this value.
    * 
    * @author Kevin Harris <kpharris@users.sourceforge.net>
-   * @version $Revision: 1.3 $
+   * @version $Revision: 1.4 $
    * 
    */
   template<class T>
@@ -223,29 +223,31 @@ namespace amethyst
     typedef const_simple_iterator<T> const_iterator;
     typedef std::reverse_iterator<iterator> reverse_iterator;
     typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
-
+    typedef T value_type;
+    typedef T& reference;
+    
   private:
     T* data_pointer;
-    size_t data_size;
-    size_t storage_size;
+    T* end_pointer;
+    T* storage_end;
 
     /**
-     * Call the destructor for the first count items, but leave the current
-     * data pointer intact. 
+     * Call the destructor for all currently existing array elements, but leave
+     * the current data pointer intact.
      */
-    void destroy_data(size_t count);
+    void destroy_data();
 
     /**
      * Remove the data pointer, setting things to a known state.
      */
     void remove_storage();
 
-    enum resize_type { E_ABSOLUTE, E_NORMAL };
+    enum resize_type { E_ABSOLUTE_RESIZE, E_NORMAL_RESIZE };
     
     /**
      * Increase the storage space in the vector.
      */
-    void increase_storage(size_t desired, resize_type size_method = E_ABSOLUTE);
+    void increase_storage(size_t desired, resize_type size_method = E_ABSOLUTE_RESIZE);
     
   protected:
 
@@ -269,30 +271,30 @@ namespace amethyst
     quick_vector& operator= (const quick_vector& old);
 
     /** Subscript operator (not size checked) */
-    T& operator[](size_t index)
+    inline T& operator[](size_t index)
     {
       return data_pointer[index];
     }
     /** Subscript operator (not size checked) */
-    T operator[](size_t index) const
+    inline T operator[](size_t index) const
     {
       return data_pointer[index];
     }
     /** Returns the size of the vector */
-    size_t size() const
+    inline size_t size() const
     {
-      return data_size;
+      return end() - begin();
     }
 
-    iterator begin() { return iterator(data_pointer); }
-    iterator end() { return iterator(data_pointer + data_size); }
-    const_iterator begin() const { return const_iterator(data_pointer); }
-    const_iterator end() const { return const_iterator(data_pointer + data_size); }
+    inline iterator begin() { return iterator(data_pointer); }
+    inline iterator end() { return iterator(end_pointer); }
+    inline const_iterator begin() const { return const_iterator(data_pointer); }
+    inline const_iterator end() const { return const_iterator(end_pointer); }
     
-    reverse_iterator rbegin() { return reverse_iterator(end()); }
-    reverse_iterator rend() { return reverse_iterator(begin()); }
-    const_reverse_iterator rbegin() const { return const_reverse_iterator(end()); }
-    const_reverse_iterator rend() const { return const_reverse_iterator(begin()); }
+    inline reverse_iterator rbegin() { return reverse_iterator(end()); }
+    inline reverse_iterator rend() { return reverse_iterator(begin()); }
+    inline const_reverse_iterator rbegin() const { return const_reverse_iterator(end()); }
+    inline const_reverse_iterator rend() const { return const_reverse_iterator(begin()); }
 
     void append(const T& val);
     void append(const quick_vector<T>& vec);
@@ -310,33 +312,35 @@ namespace amethyst
   template<class T>
   inline quick_vector<T>::quick_vector():
     data_pointer(NULL),
-    data_size(0),
-    storage_size(0)
+    end_pointer(NULL),
+    storage_end(NULL)
   {
-  
+    const size_t minimum_size = 16;
+    increase_storage(minimum_size, E_ABSOLUTE_RESIZE);
   } // quick_vector()
 
 
   template<class T>
-  inline void quick_vector<T>::destroy_data(size_t count)
+  inline void quick_vector<T>::destroy_data()
   {
     if( data_pointer )
     {
-      for( size_t i = 0; i < count; ++i )
+      for( T* ptr = data_pointer; ptr != end_pointer; ++ptr )
       {
-        data_pointer[i].~T();
+        ptr->~T();
       }
     }
-    data_size = 0;
+    end_pointer = data_pointer;
   }
 
   template<class T>
   inline void quick_vector<T>::remove_storage()  
   {
-    destroy_data(data_size);
+    destroy_data();
     delete[] reinterpret_cast<char*>(data_pointer);
     data_pointer = NULL;
-    storage_size = 0;
+    end_pointer = NULL;
+    storage_end = NULL;
   }
   
   //-------------------------------------------
@@ -345,8 +349,8 @@ namespace amethyst
   template<class T>
   inline quick_vector<T>::quick_vector(size_t size):
     data_pointer(NULL),
-    data_size(0),
-    storage_size(0)
+    end_pointer(NULL),
+    storage_end(NULL)
   {
     increase_storage(size);
 
@@ -354,9 +358,9 @@ namespace amethyst
     try
 #endif
     {
-      for( data_size = 0; data_size < size; ++data_size )
+      for( end_pointer = data_pointer; end_pointer != storage_end; ++end_pointer )
       {
-        new(&data_pointer[data_size]) T();
+        new(end_pointer) T();
       }
     }
 #if defined(AMETHYST_EXCEPTION_SAFETY)
@@ -378,17 +382,17 @@ namespace amethyst
   template<class T>
   inline quick_vector<T>::quick_vector(size_t size, const T& initial_value):
     data_pointer(NULL),
-    data_size(0),
-    storage_size(0)
+    end_pointer(NULL),
+    storage_end(NULL)
   { 
-    increase_storage(size);   
+    increase_storage(size, E_ABSOLUTE_RESIZE);
 #if defined(AMETHYST_EXCEPTION_SAFETY)
     try
 #endif
     {
-      for( data_size = 0 ; data_size < size; ++data_size )
+      for( end_pointer = data_pointer; end_pointer != storage_end; ++end_pointer )
       {
-        new(&data_pointer[data_size]) T(initial_value);
+        new(end_pointer) T(initial_value);
       }
     }
 #if defined(AMETHYST_EXCEPTION_SAFETY)
@@ -419,20 +423,31 @@ namespace amethyst
   template<class T>
   inline quick_vector<T>::quick_vector(const quick_vector<T>& old):
     data_pointer(NULL),
-    data_size(0),
-    storage_size(0)
+    end_pointer(NULL),
+    storage_end(NULL)
   {
     if( old.data_pointer )
     {
-      increase_storage(old.data_size);
+      size_t desired_size = old.size();
+      size_t next_power = 1;
+
+      while(next_power < desired_size)
+      {
+        next_power <<= 1;
+      }
+      desired_size = next_power;
+      
+      increase_storage(desired_size);
 
 #if defined(AMETHYST_EXCEPTION_SAFETY)
       try
 #endif
       {
-        for( data_size = 0; data_size < old.data_size; ++data_size )
+        T const* old_ptr = old.data_pointer;
+        T const* old_end = old.end_pointer;
+        for( end_pointer = data_pointer; old_ptr != old_end; ++end_pointer, ++old_ptr)
         {
-          new(&data_pointer[data_size]) T(old.data_pointer[data_size]);
+          new(end_pointer) T(*old_ptr);
         }
       }
 #if defined(AMETHYST_EXCEPTION_SAFETY)
@@ -458,11 +473,20 @@ namespace amethyst
     if( &old != this)
     {
       // Remove the current data (leave the storage alone).
-      destroy_data(data_size);
+      destroy_data();
 
+      size_t desired_size = old.size();
+      size_t next_power = 1;
+
+      while(next_power < desired_size)
+      {
+        next_power <<= 1;
+      }
+      desired_size = next_power;
+      
       // Increase the storage (if required) to hold all of the data to be
       // copied.
-      increase_storage(old.data_size);
+      increase_storage(desired_size);
 
       // NOTE: This assumes that the data sizes are valid when the pointers
       // are non-null, and that when the pointers are NULL, the data size is
@@ -473,9 +497,11 @@ namespace amethyst
       // the data in good state (up until the final item that threw), so
       // data_size worth of elements can have the destructor called, should any
       // exception be thrown from a copy constructor..
-      for( data_size = 0; data_size < old.data_size; ++data_size )
+      T const* old_ptr = old.data_pointer;
+      T const* old_end = old.end_pointer;
+      for( end_pointer = data_pointer; old_ptr != old_end; ++end_pointer, ++old_ptr)
       {
-        new(&data_pointer[data_size]) T(old.data_pointer[data_size]);
+        new(end_pointer) T(*old_ptr);
       }
     }
     return (*this);
@@ -486,40 +512,21 @@ namespace amethyst
   {
     // NOTE: Size cannot decrease, so the only other case is equal, in which we
     // don't need any more memory.
-    if( desired > storage_size )
+    size_t current_size = storage_end - data_pointer;
+    if( desired > current_size )
     {
       size_t new_size = desired;
-      if( size_method == E_NORMAL )
+      if( size_method == E_NORMAL_RESIZE )
       {
-        // If less than 1k, double the size.  If larger, be more conservative.
-        if( storage_size * sizeof(T) < (AMETHYST_VECTOR_INCREMENT_SIZE) )
+        new_size = current_size << 1;
+
+        if( new_size < desired )
         {
-          new_size = storage_size * 2;
-
-          if( new_size < desired )
-          {
-            new_size = desired;
-          }
-        }
-        else
-        {
-          size_t current_bytes = storage_size * sizeof(T);
-          size_t desired_bytes = desired * sizeof(T);
-
-          // Allocate even kilobytes.
-          size_t current_kilobytes = ((current_bytes >> 10) +
-                                      ((current_bytes & 0x3FF)?(1):(0)));
-
-          size_t desired_kilobytes = ((desired_bytes >> 10) +
-                                      ((desired_bytes & 0x3FF)?(1):(0)));
-
-
-          new_size = ( std::max(current_kilobytes + ((AMETHYST_VECTOR_INCREMENT_SIZE) >> 10), desired_kilobytes ) << 10 ) / sizeof(T);
+          new_size = desired;
         }
       }
       T* new_ptr = reinterpret_cast<T*>(new char[sizeof(T) * new_size]);
-
-      size_t i = 0;
+      T* new_end = new_ptr;
 
       // NOTE: this exception block is very important if containing objects
       // can allocate memory, or other resources.  Otherwise, there will be a
@@ -528,9 +535,9 @@ namespace amethyst
       try
 #endif
       {
-        for( ; i < data_size; ++i )
+        for( T* old_iter = data_pointer; old_iter != end_pointer; ++old_iter, ++new_end)
         {
-          new(&new_ptr[i]) T(data_pointer[i]);
+          new(new_end) T(*old_iter);
         }
       }
 #if defined(AMETHYST_EXCEPTION_SAFETY)
@@ -538,58 +545,66 @@ namespace amethyst
       {
         // Note that this only cleans up local storage.  The class storage will
         // be freed when the destructor is called.
-        for( ; i > 0; --i )
+        for(T* new_iter = new_ptr; new_iter != new_end; ++new_iter)
         {
-          new_ptr[i - 1].~T()
-            }
+          new_iter->~T();
+        }
 
-        delete[] static_cast<char*>(new_ptr);
+        delete[] reinterpret_cast<char*>(new_ptr);
 
         throw;
       }
 #endif      
-      size_t current_size = data_size;
       remove_storage();
       data_pointer = new_ptr;
-      data_size = current_size;
-      storage_size = new_size;      
+      end_pointer = new_end;
+      storage_end = data_pointer + new_size;
     }
   }
 
   template <class T>
   inline void quick_vector<T>::append(const T& val)
   {
-    size_t new_size = data_size + 1;
-
-    increase_storage(new_size, E_NORMAL);
-
-    new(&data_pointer[data_size++]) T(val);
+    // Call the increase_storage function if there's not enough space.
+    if( end_pointer >= storage_end )
+    {
+      size_t new_size = size() + 1;
+      
+      increase_storage(new_size, E_NORMAL_RESIZE);
+    }
+    new(end_pointer++) T(val);    
   }
 
   template <class T>
   inline void quick_vector<T>::append(const quick_vector<T>& vec)
   {
-    size_t new_size = data_size + vec.data_size;
-    size_t current_size = data_size;
-    increase_storage(new_size, E_NORMAL);
-    
-    for( ; data_size < new_size; ++data_size )
+    // Call the increase_storage function if there's not enough space.
+    T* new_end = end_pointer + vec.size();
+    if( new_end >= storage_end )
     {
-      new(&data_pointer[data_size]) T(vec.data_pointer[data_size - current_size]);
+      size_t new_size = size() + vec.size();
+      increase_storage(new_size, E_NORMAL_RESIZE);
+    }
+
+    const T* copy_end = vec.end_pointer;
+    
+    for( const T* copy_iter = vec.data_pointer; copy_iter != copy_end; ++copy_iter, ++end_pointer)
+    {
+      new(end_pointer) T(*copy_iter);
     }
   }
 
   template<class T>
-    inline void quick_vector<T>::push_back(const T& val)
-    {
-      append(val);
-    }
+  inline void quick_vector<T>::push_back(const T& val)
+  {
+    append(val);
+  }
   
   template<class T>
-    inline void quick_vector<T>::push_back(const quick_vector<T>& vec)
-    {
-      append(vec);
-    }
+  inline void quick_vector<T>::push_back(const quick_vector<T>& vec)
+  {
+    append(vec);
+  }
   
 } // namespace amethyst
 
