09effa54cefc07162a77ada8cfb39e4a83560d02
kpharris <kpharris>
2006-01-30 11:18:10 +0000

    Altering the tests so they all require semicolons, and can't interfere with external try/catch or if/else blocks (when misused).

--- test/testinclude.hpp
+++ test/testinclude.hpp
@@ -1,21 +1,21 @@
 /*
- * $Id: testinclude.hpp,v 1.1 2003/09/16 09:04:06 kpharris Exp $
+ * $Id: testinclude.hpp,v 1.2 2006/01/30 11:18:10 kpharris Exp $
  *
  * Part of "Amethyst Palace" - A playground for future graphics ideas
  * Copyright (C) 2003 Kevin Harris
  *
- * This program is free software; you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation; either version 2 of the License, or    
- * (at your option) any later version.                                  
- *                                                                      
- * This program is distributed in the hope that it will be useful, but  
- * WITHOUT ANY WARRANTY; without even the implied warranty of           
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    
- * General Public License for more details.                             
- *                                                                      
- * You should have received a copy of the GNU General Public License    
- * along with this program; if not, write to the Free Software          
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  */
 
@@ -29,49 +29,63 @@
  * program.
  */
 
-#define TEST_EXCEPTION_TEST(try_block, catch_block)               \
-try                                                               \
-{                                                                 \
-  try_block;                                                      \
-}                                                                 \
-catch(...)                                                        \
-{                                                                 \
-  catch_block;                                                    \
-}
+#define TEST_EXCEPTION_TEST(try_block, catch_block) \
+   do                                               \
+   {                                                \
+      try                                           \
+      {                                             \
+         try_block;                                 \
+      }                                             \
+      catch(...)                                    \
+      {                                             \
+         catch_block;                               \
+      }                                             \
+   }                                                \
+   while(0)
 
-#define TEST_EXCEPTION_CATCH(try_block, catch_def, catch_block)   \
-try                                                               \
-{                                                                 \
-  try_block;                                                      \
-}                                                                 \
-catch(catch_def)                                                  \
-{                                                                 \
-  catch_block;                                                    \
-}
+#define TEST_EXCEPTION_CATCH(try_block, catch_def, catch_block) \
+   do                                                           \
+   {                                                            \
+      try                                                       \
+      {                                                         \
+         try_block;                                             \
+      }                                                         \
+      catch(catch_def)                                          \
+      {                                                         \
+         catch_block;                                           \
+      }                                                         \
+   }                                                            \
+   while(0)
 
 #define TEST_EXCEPTION_NAMED(name, try_block, not_thrown, thrown) \
-std::cout << "Trying " << name << "... ";                         \
-try                                                               \
-{                                                                 \
-  try_block;                                                      \
-  std::cout << not_thrown << std::endl;                           \
-}                                                                 \
-catch(...)                                                        \
-{                                                                 \
-  std::cout << thrown << std::endl;                               \
-}
+   do                                                             \
+   {                                                              \
+      std::cout << "Trying " << name << "... ";                   \
+      try                                                         \
+      {                                                           \
+         try_block;                                               \
+         std::cout << not_thrown << std::endl;                    \
+      }                                                           \
+      catch(...)                                                  \
+      {                                                           \
+         std::cout << thrown << std::endl;                        \
+      }                                                           \
+   }                                                              \
+   while(0)
 
-#define TEST_RESULT_NAMED(name, statement, if_true, if_false)     \
-std::cout << "Checking " << name << "... ";                       \
-if( statement )                                                   \
-{                                                                 \
-  std::cout << if_true << std::endl;                              \
-}                                                                 \
-else                                                              \
-{                                                                 \
-  std::cout << if_false << std::endl;                             \
-}
-
+#define TEST_RESULT_NAMED(name, statement, if_true, if_false) \
+   do                                                         \
+   {                                                          \
+      std::cout << "Checking " << name << "... ";             \
+      if( statement )                                         \
+      {                                                       \
+         std::cout << if_true << std::endl;                   \
+      }                                                       \
+      else                                                    \
+      {                                                       \
+         std::cout << if_false << std::endl;                  \
+      }                                                       \
+   }                                                          \
+   while(0)
 
 #endif /* !defined(__TESTINCLUDE_HPP__) */
-
