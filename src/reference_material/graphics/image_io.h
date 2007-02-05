4b5a28595b4fd3e59d76924027b56e35895ea5de
kpharris <kpharris>
2007-02-05 07:21:53 +0000

    Altering the image io to use images instead of rasters.  Adding common functions for writing bytes to streams and streambufs.

--- src/reference_material/graphics/image_io.h
+++ src/reference_material/graphics/image_io.h
@@ -3,15 +3,16 @@
 /*
   imageout.h
   This is intended as a way to convert a raster to an image that can be output
-  to a file or whatever. 
-   
- */
+  to a file or whatever.
+
+*/
 /*
   Revision history:
   19jan1999 Created this file.
- */
+*/
 
-#include <raster.hpp>
+#include <graphics/image.hpp>
+#include <graphics/image_converter.hpp>
 #include <string>
 #include <fstream>
 #include <rc_pointer.hpp>
@@ -19,37 +20,48 @@
 namespace amethyst
 {
 
-  template <class T>
-  class image_io
-  {
-  public:
-    image_io()
-    {
-    }
-    virtual ~image_io()
-    {
-    }
+	template <class T>
+	class image_io
+	{
+	public:
+		image_io()
+		{
+		}
+		virtual ~image_io()
+		{
+		}
 
-    bool output(const std::string& filename, const raster<T>& source) const
-    {
-      std::ofstream o(filename.c_str());
-      return output(o, source);    
-    }
-    bool output(const std::string& filename, const raster<T>& source, T gamma) const
-    {
-      std::ofstream o(filename.c_str());
-      return output(o, source, gamma);    
-    }
-    virtual bool output(std::ostream& o, const raster<T>& source, T gamma) const = 0;
-    virtual bool output(std::ostream& o, const raster<T>& source) const = 0;
-    rc_pointer<raster<T> > input(const std::string& filename) const
-    {
-      std::ifstream i(filename.c_str());
-      return input(i);
-    }  
-    virtual rc_pointer<raster<T> > input(std::istream& i) const = 0;
-  protected:
-  };
+		bool output(const std::string& filename, const image<T>& source) const
+		{
+			std::ofstream o(filename.c_str());
+			return output(o, source);
+		}
+		bool output(const std::string& filename, const image<T>& source, T gamma) const
+		{
+			std::ofstream o(filename.c_str());
+			return output(o, source, gamma);
+		}
+		virtual bool output(std::ostream& o, const image<T>& source, T gamma) const = 0;
+		virtual bool output(std::ostream& o, const image<T>& source) const = 0;
+
+		void output_byte(std::streambuf& stream, unsigned char data) const
+		{
+			stream.sputc(std::streambuf::char_type(data));
+		}
+		void output_byte(std::ostream& stream, unsigned char data) const
+		{
+			stream.put(data);
+		}
+
+
+		rc_pointer<image<T> > input(const std::string& filename) const
+		{
+			std::ifstream i(filename.c_str());
+			return input(i);
+		}
+		virtual rc_pointer<image<T> > input(std::istream& i) const = 0;
+	protected:
+	};
 
 } // namespace amethyst
 
