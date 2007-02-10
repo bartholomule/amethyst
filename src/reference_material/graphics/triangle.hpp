d75f2a81dca348726ad646ecd708ac824e43b28e
kpharris <kpharris>
2007-02-10 13:14:19 +0000

    Fixing an include.  Reindenting.

--- src/reference_material/graphics/triangle.hpp
+++ src/reference_material/graphics/triangle.hpp
@@ -1,21 +1,21 @@
 /*
- * $Id: triangle.hpp,v 1.8 2004/06/01 03:59:31 kpharris Exp $
+ * $Id: triangle.hpp,v 1.9 2007/02/10 13:14:19 kpharris Exp $
  *
  * Part of "Amethyst" a playground for graphics development
  * Copyright (C) 2004 Kevin Harris
  *
- * This program is free software; you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation; either version 2 of the License, or    
- * (at your option) any later version.                                  
- *                                                                      
- * This program is distributed in the hope that it will be useful, but  
- * WITHOUT ANY WARRANTY; without even the implied warranty of           
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    
- * General Public License for more details.                             
- *                                                                      
- * You should have received a copy of the GNU General Public License    
- * along with this program; if not, write to the Free Software          
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  */
 
@@ -25,289 +25,289 @@
 // --------------------------------------
 // Include of parent class header
 // --------------------------------------
-#include "plane.hpp"
+#include <graphics/plane.hpp>
 
 namespace amethyst
-{ 
+{
 
-  /**
-   * 
-   * A simple triangle class, which is based on the plane class.
-   * 
-   * @author Kevin Harris <kpharris@users.sourceforge.net>
-   * @version $Revision: 1.8 $
-   * 
-   */
-  template<class T>
-  class triangle : public plane<T>
-  {
+	/**
+	 *
+	 * A simple triangle class, which is based on the plane class.
+	 *
+	 * @author Kevin Harris <kpharris@users.sourceforge.net>
+	 * @version $Revision: 1.9 $
+	 *
+	 */
+	template<class T>
+	class triangle : public plane<T>
+	{
 
-  private:
+	private:
 
-  protected:
+	protected:
 
-  public:
-    /** Default constructor */
-    triangle();
+	public:
+		/** Default constructor */
+		triangle();
 
-    triangle(const point3<T>& corner1 = point3<T>(0,0,0),
-             const point3<T>& corner2 = point3<T>(1,0,0),
-             const point3<T>& corner3 = point3<T>(0,1,0));
+		triangle(const point3<T>& corner1 = point3<T>(0,0,0),
+			const point3<T>& corner2 = point3<T>(1,0,0),
+			const point3<T>& corner3 = point3<T>(0,1,0));
 
-    /** Destructor */
-    virtual ~triangle();
+		/** Destructor */
+		virtual ~triangle();
 
-    /** Copy constructor */
-    triangle(const triangle& old);
+		/** Copy constructor */
+		triangle(const triangle& old);
 
-    /** Assignment operator */
-    triangle& operator= (const triangle& old);
+		/** Assignment operator */
+		triangle& operator= (const triangle& old);
 
-    /** Returns if the given point is inside the shape. */
-    virtual bool inside(const point3<T>& p) const;
+		/** Returns if the given point is inside the shape. */
+		virtual bool inside(const point3<T>& p) const;
 
-    /** Returns if the given sphere intersects the shape. */
-    virtual bool intersects(const sphere<T>& s) const;
+		/** Returns if the given sphere intersects the shape. */
+		virtual bool intersects(const sphere<T>& s) const;
 
-    /** Returns if the given plane intersects the shape. */
-    virtual bool intersects(const plane<T>& p) const;
+		/** Returns if the given plane intersects the shape. */
+		virtual bool intersects(const plane<T>& p) const;
 
-    /** Returns if the given line intersects the plane. */
-    virtual bool intersects_line(const unit_line3<T>& line,
-                                 intersection_info<T>& intersection,
-                                 const intersection_requirements& requirements) const;
-
-
-    /**
-     * A quick intersection test.  This will calculate nothing but the
-     * distance. This is most useful for shadow tests, and other tests where no
-     * textures will be applied.
-     */ 
-    virtual bool quick_intersection(const unit_line3<T>& line,
-				    T time, T& distance) const;
-    
-    virtual std::string internal_members(const std::string& indentation, bool prefix_with_classname = false) const;
-    
-    virtual std::string to_string(const std::string& indent,
-                                  const std::string& level_indent = "  ") const;
-    virtual std::string name() const
-    {
-      return "triangle";
-    }
-
-    virtual intersection_capabilities get_intersection_capabilities() const;
-    virtual object_capabilities get_object_capabilities() const;
-    
-  }; // class triangle
+		/** Returns if the given line intersects the plane. */
+		virtual bool intersects_line(const unit_line3<T>& line,
+			intersection_info<T>& intersection,
+			const intersection_requirements& requirements) const;
 
 
+		/**
+		 * A quick intersection test.  This will calculate nothing but the
+		 * distance. This is most useful for shadow tests, and other tests where no
+		 * textures will be applied.
+		 */
+		virtual bool quick_intersection(const unit_line3<T>& line,
+			T time, T& distance) const;
 
-  //---------------------------------------
-  // Default constructor for class triangle
-  //---------------------------------------
-  template<class T>
-  triangle<T>::triangle():
-    plane<T>()
-  {
+		virtual std::string internal_members(const std::string& indentation, bool prefix_with_classname = false) const;
 
-  } // triangle()
+		virtual std::string to_string(const std::string& indent,
+			const std::string& level_indent = "  ") const;
+		virtual std::string name() const
+		{
+			return "triangle";
+		}
 
-  template<class T>
-  triangle<T>::triangle(const point3<T>& corner1,
-                        const point3<T>& corner2,
-                        const point3<T>& corner3):
-    plane<T>(corner1, corner2, corner3)
-  {
-  } // triangle(point3,point3,point3)
+		virtual intersection_capabilities get_intersection_capabilities() const;
+		virtual object_capabilities get_object_capabilities() const;
+
+	}; // class triangle
 
 
-  //------------------------------
-  // Destructor for class triangle
-  //------------------------------
-  template<class T>
-  triangle<T>::~triangle()
-  {
 
-  } // ~triangle()
+	//---------------------------------------
+	// Default constructor for class triangle
+	//---------------------------------------
+	template<class T>
+	triangle<T>::triangle():
+		plane<T>()
+	{
 
-  //------------------------------------
-  // Copy constructor for class triangle
-  //------------------------------------
-  template<class T>
-  triangle<T>::triangle(const triangle<T>& old):
-    plane<T>(old)
-  {
+	} // triangle()
 
-  } // triangle(triangle)
+	template<class T>
+	triangle<T>::triangle(const point3<T>& corner1,
+		const point3<T>& corner2,
+		const point3<T>& corner3):
+		plane<T>(corner1, corner2, corner3)
+	{
+	} // triangle(point3,point3,point3)
 
-  //---------------------------------------
-  // Assignment operator for class triangle
-  //---------------------------------------
-  template<class T>
-  triangle<T>& triangle<T>::operator= (const triangle<T>& old)
-  {
-    // Generic check for self-assignment
-    if( &old != this )
-    {
-      plane<T>::operator=(old);
-    }
-    return(*this);
-  } // triangle::operator=(triangle)
 
-  template<class T>
-  bool triangle<T>::inside(const point3<T>& point) const
-  {
-    coord2<T> uv;
-    if( plane<T>::extract_uv_for_point(point, uv) )
-    {
-      if( (uv.x() > 0 && uv.y() > 0) &&
-          (uv.x() + uv.y() < 1) )
-      {
-        return true;
-      }
-    }
-    return false;
-  }
+	//------------------------------
+	// Destructor for class triangle
+	//------------------------------
+	template<class T>
+	triangle<T>::~triangle()
+	{
 
-  template<class T>
-  bool triangle<T>::intersects(const sphere<T>& s) const
-  {
-    // If any of the 3 corners is inside the sphere, the triangle intersects.
-    if( s.inside( plane<T>::get_origin() ) ||
-        s.inside( plane<T>::get_origin() + plane<T>::get_u_vector() ) ||
-        s.inside( plane<T>::get_origin() + plane<T>::get_v_vector() ) )
-    {
-      return true;
-    }
-    return false;
-  }
+	} // ~triangle()
 
-  template<class T>
-  bool triangle<T>::intersects(const plane<T>& p) const
-  {
-    T unused;
-    const point3<T>& pp = plane<T>::get_origin();
-    point3<T> pu = plane<T>::get_origin() + plane<T>::get_u_vector();
-    point3<T> pv = plane<T>::get_origin() + plane<T>::get_v_vector();
+	//------------------------------------
+	// Copy constructor for class triangle
+	//------------------------------------
+	template<class T>
+	triangle<T>::triangle(const triangle<T>& old):
+		plane<T>(old)
+	{
 
-    if( p.quick_intersection( unit_line3<T>(pp, pu), 0, unused ) ||
-        p.quick_intersection( unit_line3<T>(pp, pv), 0, unused ) ||
-        p.quick_intersection( unit_line3<T>(pu, pv), 0, unused ) )
-    {
-      return true;
-    }
+	} // triangle(triangle)
 
-    return false;
-  }
+	//---------------------------------------
+	// Assignment operator for class triangle
+	//---------------------------------------
+	template<class T>
+	triangle<T>& triangle<T>::operator= (const triangle<T>& old)
+	{
+		// Generic check for self-assignment
+		if( &old != this )
+		{
+			plane<T>::operator=(old);
+		}
+		return(*this);
+	} // triangle::operator=(triangle)
 
-  template<class T>
-  bool triangle<T>::intersects_line(const unit_line3<T>& line,
-                                    intersection_info<T>& intersection,
-                                    const intersection_requirements& requirements) const
-  {
-    intersection_info<T> temp_intersection;
+	template<class T>
+	bool triangle<T>::inside(const point3<T>& point) const
+	{
+		coord2<T> uv;
+		if( plane<T>::extract_uv_for_point(point, uv) )
+		{
+			if( (uv.x() > 0 && uv.y() > 0) &&
+				(uv.x() + uv.y() < 1) )
+			{
+				return true;
+			}
+		}
+		return false;
+	}
 
-    intersection_requirements temp_requirements = requirements;
-    temp_requirements.force_uv(true);
-    
-    if( plane<T>::intersects_line(line, temp_intersection, temp_requirements) )
-    {
+	template<class T>
+	bool triangle<T>::intersects(const sphere<T>& s) const
+	{
+		// If any of the 3 corners is inside the sphere, the triangle intersects.
+		if( s.inside( plane<T>::get_origin() ) ||
+			s.inside( plane<T>::get_origin() + plane<T>::get_u_vector() ) ||
+			s.inside( plane<T>::get_origin() + plane<T>::get_v_vector() ) )
+		{
+			return true;
+		}
+		return false;
+	}
 
-      if( temp_intersection.have_uv() )
-      {
-        coord2<T> uv = temp_intersection.get_uv();
-        
-        if( (uv.x() > 0 && uv.y() > 0) &&
-            (uv.x() + uv.y() < 1) )
-        {
-          intersection = temp_intersection;
-          
-          // The requirements should be handled by the plane.
-          
-          return true;
-        }
-      }
-      else
-      {
-        std::cout << "ERROR! We should have a UV, but none was provided!" << std::endl;
-      }
-    }
-    return false;
-  }
+	template<class T>
+	bool triangle<T>::intersects(const plane<T>& p) const
+	{
+		T unused;
+		const point3<T>& pp = plane<T>::get_origin();
+		point3<T> pu = plane<T>::get_origin() + plane<T>::get_u_vector();
+		point3<T> pv = plane<T>::get_origin() + plane<T>::get_v_vector();
 
-  template <class T>
-  std::string triangle<T>::internal_members(const std::string& indentation, bool prefix_with_classname) const
-  {
-    std::string retval;
-    std::string internal_tagging = indentation;
+		if( p.quick_intersection( unit_line3<T>(pp, pu), 0, unused ) ||
+			p.quick_intersection( unit_line3<T>(pp, pv), 0, unused ) ||
+			p.quick_intersection( unit_line3<T>(pu, pv), 0, unused ) )
+		{
+			return true;
+		}
 
-    retval += plane<T>::internal_members(indentation, true);
-    
-    if( prefix_with_classname )
-    {
-      internal_tagging += triangle<T>::name() + "::";
-    }
+		return false;
+	}
 
-    return retval;
-  }
+	template<class T>
+	bool triangle<T>::intersects_line(const unit_line3<T>& line,
+		intersection_info<T>& intersection,
+		const intersection_requirements& requirements) const
+	{
+		intersection_info<T> temp_intersection;
 
-  template <class T>
-  std::string triangle<T>::to_string(const std::string& indent,
-                                     const std::string& level_indent) const
-  {
-    return ( indent + "triangle\n" +
-             indent + "{\n" +
-             triangle<T>::internal_members(indent + level_indent, false) +
-             indent + "}" );
-  }
+		intersection_requirements temp_requirements = requirements;
+		temp_requirements.force_uv(true);
 
-  template <class T>
-  intersection_capabilities triangle<T>::get_intersection_capabilities() const
-  {
-    intersection_capabilities caps = plane<T>::get_intersection_capabilities();
+		if( plane<T>::intersects_line(line, temp_intersection, temp_requirements) )
+		{
 
-    return caps;
-  }
-  
-  template <class T>
-  object_capabilities triangle<T>::get_object_capabilities() const
-  {
-    object_capabilities caps = plane<T>::get_object_capabilities();
+			if( temp_intersection.have_uv() )
+			{
+				coord2<T> uv = temp_intersection.get_uv();
 
-    caps &= ~object_capabilities::INFINITE;
-    caps |= object_capabilities::BOUNDABLE;
-    caps |= object_capabilities::POLYGONIZATION;
-    
-    return caps;    
-  }
+				if( (uv.x() > 0 && uv.y() > 0) &&
+					(uv.x() + uv.y() < 1) )
+				{
+					intersection = temp_intersection;
 
-  template <class T>
-  bool triangle<T>::quick_intersection(const unit_line3<T>& line, T time, T& distance) const
-  {
-    T temp_distance;
-    
-    if( plane<T>::quick_intersection(line, time, temp_distance) )
-    {
-      point3<T> p = (temp_distance * line.direction() + line.origin());
-      coord2<T> uv;
+					// The requirements should be handled by the plane.
 
-      // Note that a non-checked version is being called, because the point
-      // *SHOULD* be on the plane (it hit it, why check it?).  This improves
-      // speed, and reduces artifacts with expanded error (a point that is on
-      // the plane would sometimes not show as being on the plane -- this is
-      // the case when a small epsilon and single-precision floats are used. 
-      plane<T>::extract_uv_for_point_nonchecked(p, uv);
+					return true;
+				}
+			}
+			else
+			{
+				std::cout << "ERROR! We should have a UV, but none was provided!" << std::endl;
+			}
+		}
+		return false;
+	}
 
-      if( (uv.x() > 0 && uv.y() > 0) &&
-          (uv.x() + uv.y() < 1) )
-      {
-        distance = temp_distance;
-        return true;
-      }
-    }
-    return false;    
-  }
-  
+	template <class T>
+	std::string triangle<T>::internal_members(const std::string& indentation, bool prefix_with_classname) const
+	{
+		std::string retval;
+		std::string internal_tagging = indentation;
+
+		retval += plane<T>::internal_members(indentation, true);
+
+		if( prefix_with_classname )
+		{
+			internal_tagging += triangle<T>::name() + "::";
+		}
+
+		return retval;
+	}
+
+	template <class T>
+	std::string triangle<T>::to_string(const std::string& indent,
+		const std::string& level_indent) const
+	{
+		return ( indent + "triangle\n" +
+			indent + "{\n" +
+			triangle<T>::internal_members(indent + level_indent, false) +
+			indent + "}" );
+	}
+
+	template <class T>
+	intersection_capabilities triangle<T>::get_intersection_capabilities() const
+	{
+		intersection_capabilities caps = plane<T>::get_intersection_capabilities();
+
+		return caps;
+	}
+
+	template <class T>
+	object_capabilities triangle<T>::get_object_capabilities() const
+	{
+		object_capabilities caps = plane<T>::get_object_capabilities();
+
+		caps &= ~object_capabilities::INFINITE;
+		caps |= object_capabilities::BOUNDABLE;
+		caps |= object_capabilities::POLYGONIZATION;
+
+		return caps;
+	}
+
+	template <class T>
+	bool triangle<T>::quick_intersection(const unit_line3<T>& line, T time, T& distance) const
+	{
+		T temp_distance;
+
+		if( plane<T>::quick_intersection(line, time, temp_distance) )
+		{
+			point3<T> p = (temp_distance * line.direction() + line.origin());
+			coord2<T> uv;
+
+			// Note that a non-checked version is being called, because the point
+			// *SHOULD* be on the plane (it hit it, why check it?).  This improves
+			// speed, and reduces artifacts with expanded error (a point that is on
+			// the plane would sometimes not show as being on the plane -- this is
+			// the case when a small epsilon and single-precision floats are used.
+			plane<T>::extract_uv_for_point_nonchecked(p, uv);
+
+			if( (uv.x() > 0 && uv.y() > 0) &&
+				(uv.x() + uv.y() < 1) )
+			{
+				distance = temp_distance;
+				return true;
+			}
+		}
+		return false;
+	}
+
 } // namespace amethyst
 
 
