aaf3353bb00814d09fcc9176424b0e0ff9f15862
Kevin Harris <bartholomule@gmail.com>
2018-04-05 14:03:26 -0600

    Fixing the test framework to allow tests from outside the test body
    Example:
      void test_something() { TEST_COMPARE_EQUAL(1,1); }
      AUTO_UNIT_TEST(foo) { test_something(); }

--- src/amethyst/test_framework/unit_test.cpp
+++ src/amethyst/test_framework/unit_test.cpp
@@ -1,24 +1,3 @@
-/*
- * $Id: unit_test.cpp,v 1.1 2008/06/09 16:31:18 kpharris Exp $
- *
- * Part of "Amethyst Palace" - A playground for future graphics ideas
- * Copyright (C) 2007 Kevin Harris
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
- */
-
 #include "unit_test.hpp"
 #include "unit_test_aggregator.hpp"
 #include "unit_test_exceptions.hpp"
@@ -27,6 +6,29 @@ namespace amethyst
 {
     namespace test
     {
+        namespace
+        {
+            const unit_test* active_test = nullptr;
+
+            struct active_test_scope
+            {
+                active_test_scope(const unit_test* t) : m_test(active_test)
+                {
+                    active_test = t;
+                }
+                ~active_test_scope()
+                {
+                    active_test = m_test;
+                }
+                const unit_test* m_test;
+            };
+        }
+
+        const unit_test* current_test()
+        {
+            return active_test;
+        }
+
         unit_test::unit_test(const std::string& name, test_results* chain_results)
             : result_counter(new test_results(name, chain_results))
             , test_name(name)
@@ -39,6 +41,7 @@ namespace amethyst
 
         void unit_test::run_test(const test_information& info) const
         {
+            active_test_scope t(this);
             try
             {
                 do_run_test(info);
--- src/amethyst/test_framework/unit_test.hpp
+++ src/amethyst/test_framework/unit_test.hpp
@@ -1,30 +1,9 @@
-/*
- * $Id: unit_test.hpp,v 1.2 2008/06/10 06:57:52 kpharris Exp $
- *
- * Part of "Amethyst Palace" - A playground for future graphics ideas
- * Copyright (C) 2007 Kevin Harris
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
- */
-
-#if       !defined(UNIT_TEST_HPP_INCLUDED)
-#define            UNIT_TEST_HPP_INCLUDED
+#pragma once
 
 #include "unit_test_fwd.hpp"
 #include "unit_test_exceptions.hpp"
 #include "general/string_format.hpp"
+#include "general/defines.hpp"
 #include <string>
 #include <iosfwd>
 
@@ -46,73 +25,89 @@ namespace amethyst
             void test_failed(const test_information& info, const std::string& reason) const;
 
             void print_results() const;
+            std::string test_name;
         protected:
             test_results* result_counter;
-            std::string test_name;
         private:
             virtual void do_run_test(const test_information& info) const = 0;
         };
+
+        const unit_test* current_test();
     }
 }
 
-#define TEST_BOOLEAN(body) \
+#define TEST_BODY_HELPER(name, body) \
     do { \
-        ::amethyst::test::test_information local_info = TEST_INFORMATION(::amethyst::string_format("%1_boolean_%2", test_name, __LINE__)); \
-        unit_test::test_started(local_info); \
+        ::amethyst::test::test_information local_info = TEST_INFORMATION(::amethyst::test::current_test()->test_name + "_" + name + "_" + std::to_string(__LINE__)); \
+        ::amethyst::test::current_test()->test_started(local_info); \
         if (body) \
         { \
-            unit_test::test_passed(local_info); \
+            ::amethyst::test::current_test()->test_passed(local_info); \
         } \
         else \
         { \
-            unit_test::test_failed(local_info, ::amethyst::string_format("Boolean test failed for \"%1\": (%2)", #body, (body))); \
+            ::amethyst::test::current_test()->test_failed(local_info, name + ::amethyst::string_format(" test failed for \"%1\": (%2)", #body, (body))); \
         } \
     } while (0)
 
-#define TEST_COMPARE_GENERIC(a, b, operation) \
+
+#define TEST_BOOLEAN(body) TEST_BODY_HELPER("boolean", body)
+
+#define TEST_COMPARE_GENERIC(a, b, op, invop) \
     do { \
-        ::amethyst::test::test_information local_info = TEST_INFORMATION(::amethyst::string_format("%1_comparison_%2(%3)", test_name, __LINE__,#operation)); \
-        unit_test::test_started(local_info); \
-        if ((a)operation (b)) \
+        ::amethyst::test::test_information local_info = TEST_INFORMATION("compare(" #op  ")_" + std::to_string(__LINE__)); \
+        ::amethyst::test::current_test()->test_started(local_info); \
+        const auto& aaaa = (a); \
+        const auto& bbbb = (b); \
+        if (aaaa op bbbb) \
         { \
-            unit_test::test_passed(local_info); \
+            ::amethyst::test::current_test()->test_passed(local_info); \
         } \
         else \
         { \
-            unit_test::test_failed(local_info, ::amethyst::string_format("comparison failed for \"%1 %2 %3\": (%4) %5 (%6)", #a, #operation, #b, a, #operation, b)); \
+            ::amethyst::test::current_test()->test_failed(local_info, ::amethyst::string_format(" test failed for " #a #op #b ": (%1 " #invop " %2)", aaaa, bbbb)); \
         } \
     } while (0)
 
-#define TEST_COMPARE_EQUAL(a, b) TEST_COMPARE_GENERIC(a, b, ==)
-#define TEST_COMPARE_NOT_EQUAL(a, b) TEST_COMPARE_GENERIC(a, b, !=)
-#define TEST_COMPARE_GREATER(a, b) TEST_COMPARE_GENERIC(a, b, >)
-#define TEST_COMPARE_GREATER_EQ(a, b) TEST_COMPARE_GENERIC(a, b, >=)
-#define TEST_COMPARE_LESS(a, b) TEST_COMPARE_GENERIC(a, b, <)
-#define TEST_COMPARE_LESS_EQ(a, b) TEST_COMPARE_GENERIC(a, b, <=)
+// #define TEST_COMPARE_GENERIC(a, b, operation) TEST_BODY_HELPER("compare(" #operation ")", (a) operation (b))
+
+#define TEST_COMPARE_EQUAL(a, b) TEST_COMPARE_GENERIC(a, b, ==, !=)
+#define TEST_COMPARE_NOT_EQUAL(a, b) TEST_COMPARE_GENERIC(a, b, !=, ==)
+#define TEST_COMPARE_GREATER(a, b) TEST_COMPARE_GENERIC(a, b, >, <=)
+#define TEST_COMPARE_GREATER_EQ(a, b) TEST_COMPARE_GENERIC(a, b, >=, <)
+#define TEST_COMPARE_LESS(a, b) TEST_COMPARE_GENERIC(a, b, <, >=)
+#define TEST_COMPARE_LESS_EQ(a, b) TEST_COMPARE_GENERIC(a, b, <=, >)
 
 #define TEST_COMPARE_CLOSE(a, b, epsilon) \
     do { \
-        ::amethyst::test::test_information local_info = TEST_INFORMATION(::amethyst::string_format("%1_compare_%2", test_name, __LINE__)); \
-        unit_test::test_started(local_info); \
-        if (((((a) - (b)) < (epsilon)) && (((a) - (b)) > (-(epsilon)))) \
-            || ((((b) - (a)) < (epsilon)) && (((b) - (a)) > (-(epsilon))))) \
+        ::amethyst::test::test_information local_info = TEST_INFORMATION("close_" + std::to_string(__LINE__)); \
+        ::amethyst::test::current_test()->test_started(local_info); \
+        const auto aaaa = (a); \
+        const auto bbbb = (b); \
+        if ((bbbb - aaaa) < epsilon && (aaaa - bbbb) < epsilon) \
         { \
-            unit_test::test_passed(local_info); \
+            ::amethyst::test::current_test()->test_passed(local_info); \
         } \
         else \
         { \
-            unit_test::test_failed(local_info, ::amethyst::string_format("Closeness test failed for \"%1\" and \"%2\": %3 != %4 (epsilon %5)", #a, #b, (a), (b), (epsilon))); \
+            ::amethyst::test::current_test()->test_failed(local_info, ::amethyst::string_format(" test failed for " #a "~=" #b ": (%1 != %2)", aaaa, bbbb)); \
         } \
     } while (0)
 
+#define TEST_CLOSE(a,b) TEST_COMPARE_CLOSE(a,b,AMETHYST_EPSILON)
+#define TEST_XY_EQUALS(a, p, q) TEST_COMPARE_EQUAL((a).x(), p); TEST_COMPARE_EQUAL((a).y(), q);
+#define TEST_XY_CLOSE(a, p, q) TEST_CLOSE((a).x(), p); TEST_CLOSE((a).y(), q);
+#define TEST_XYZ_EQUALS(a, p, q, r) TEST_COMPARE_EQUAL((a).x(), p); TEST_COMPARE_EQUAL((a).y(), q); TEST_COMPARE_EQUAL((a).z(), r);
+#define TEST_XYZ_CLOSE(a, p, q, r) TEST_CLOSE((a).x(), p); TEST_CLOSE((a).y(), q); TEST_CLOSE((a).z(), r);
+
 #define TEST_EXCEPTION_GENERIC_TRYBLOCK(tryblock, catchtype, catchblock, catchallblock) \
     do { \
-        ::amethyst::test::test_information local_info = TEST_INFORMATION(::amethyst::string_format("%1_exception_%2", test_name, __LINE__)); \
-        unit_test::test_started(local_info); \
+        ::amethyst::test::test_information local_info = TEST_INFORMATION(::amethyst::string_format("%1_exception_%2", ::amethyst::test::current_test()->test_name, __LINE__)); \
+        ::amethyst::test::current_test()->test_started(local_info); \
         try { tryblock } \
         catch (const ::amethyst::test::test_aborted_exception& e) \
         { \
-            unit_test::test_failed(e.info, ::amethyst::string_format("(noticed on %1:%2) Aborted: %3", __FILE__, __LINE__, e.reason)); \
+            ::amethyst::test::current_test()->test_failed(e.info, ::amethyst::string_format("(noticed on %1:%2) Aborted: %3", __FILE__, __LINE__, e.reason)); \
             throw; \
         } \
         catch (const catchtype& ex) { catchblock } \
@@ -121,34 +116,32 @@ namespace amethyst
 
 #define TEST_EXCEPTION_THROW(exception_test_body) \
     TEST_EXCEPTION_GENERIC_TRYBLOCK( \
-        { exception_test_body; unit_test::test_failed(local_info, ::amethyst::string_format("Test did not throw for \"%1\"", #exception_test_body)); }, \
+        { exception_test_body; ::amethyst::test::current_test()->test_failed(local_info, ::amethyst::string_format("Test did not throw for \"%1\"", #exception_test_body)); }, \
         ::amethyst::test::never_thrown_exception, \
         { (void)ex; }, \
-        { unit_test::test_passed(local_info); } \
+        { ::amethyst::test::current_test()->test_passed(local_info); } \
         )
 
 #define TEST_EXCEPTION_THROW_SPECIFIC(exception_test_body, exception_type) \
     TEST_EXCEPTION_GENERIC_TRYBLOCK( \
-        { exception_test_body; unit_test::test_failed(local_info, ::amethyst::string_format("Test did not throw for \"%1\"", #exception_test_body)); }, \
+        { exception_test_body; ::amethyst::test::current_test()->test_failed(local_info, ::amethyst::string_format("Test did not throw for \"%1\"", #exception_test_body)); }, \
         exception_type, \
-        { (void)ex; unit_test::test_passed(local_info); }, \
-        { unit_test::test_failed(local_info, ::amethyst::string_format("Caught unexpected exception (not %1) in \"%2\"", #exception_type, #exception_test_body)); } \
+        { (void)ex; ::amethyst::test::current_test()->test_passed(local_info); }, \
+        { ::amethyst::test::current_test()->test_failed(local_info, ::amethyst::string_format("Caught unexpected exception (not %1) in \"%2\"", #exception_type, #exception_test_body)); } \
         )
 
 #define TEST_EXCEPTION_NOTHROW(exception_test_body) \
     TEST_EXCEPTION_GENERIC_TRYBLOCK( \
-        { exception_test_body; unit_test::test_passed(local_info); }, \
+        { exception_test_body; ::amethyst::test::current_test()->test_passed(local_info); }, \
         ::amethyst::test::never_thrown_exception, \
-        { unit_test::test_failed(local_info, ::amethyst::string_format("Test threw exception in \"%1\": %2", #exception_test_body, amethyst::test::what(ex))); }, \
-        { unit_test::test_failed(local_info, ::amethyst::string_format("Test threw exception in \"%1\"", #exception_test_body)); } \
+        { ::amethyst::test::current_test()->test_failed(local_info, ::amethyst::string_format("Test threw exception in \"%1\": %2", #exception_test_body, amethyst::test::what(ex))); }, \
+        { ::amethyst::test::current_test()->test_failed(local_info, ::amethyst::string_format("Test threw exception in \"%1\"", #exception_test_body)); } \
         )
 
 #define TEST_EXCEPTION_NOTHROW_SPECIFIC(exception_test_body, exception_type) \
     TEST_EXCEPTION_GENERIC_TRYBLOCK( \
-        { exception_test_body; unit_test::test_passed(local_info); }, \
+        { exception_test_body; ::amethyst::test::current_test()->test_passed(local_info); }, \
         exception_type, \
-        { unit_test::test_failed(local_info, ::amethyst::string_format("Test threw the disallowed exception (%1) in \"%2\": %3", #exception_type, #exception_test_body, amethyst::test::what(ex))); }, \
-        { unit_test::test_passed(local_info); } \
+        { ::amethyst::test::current_test()->test_failed(local_info, ::amethyst::string_format("Test threw the disallowed exception (%1) in \"%2\": %3", #exception_type, #exception_test_body, amethyst::test::what(ex))); }, \
+        { ::amethyst::test::current_test()->test_passed(local_info); } \
         )
-
-#endif /* !defined(UNIT_TEST_HPP_INCLUDED) */
