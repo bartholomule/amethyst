555f2eae836d61b7ef2f0abea5e903e053ca4ca9
Kevin Harris <bartholomule@gmail.com>
2018-04-08 12:44:53 -0600

    Compiling the old mobius program.
    At this point, I can't remember what I was transforming with it.

--- src/amethyst/mobius.cpp
+++ src/amethyst/mobius.cpp
@@ -1,52 +1,48 @@
 // g++ -g mobius.cpp graphics/image_converter.cpp -o mobius -lm -I. -Igraphics -Igeneral -Imath
 
-#include <image.hpp>
-#include <tga_io.h>
-#include <rgbcolor.hpp>
-#include <coord2.hpp>
-#include <interval.hpp>
-#include <string_format.hpp>
+#include "graphics/image.hpp"
+#include "graphics/image_loader.hpp"
+#include "math/coord2.hpp"
+#include "math/interval.hpp"
+#include "general/string_format.hpp"
 #include <complex>
 #include <iostream>
 
 using namespace amethyst;
-using std::complex;
-using std::cerr;
-using std::endl;
 
 template <class T>
-complex<T> mobius_transform(const complex<T>& z
-                            , const complex<T>& a, const complex<T>& b
-                            , const complex<T>& c, const complex<T>& d)
+std::complex<T> mobius_transform(const std::complex<T>& z
+    , const std::complex<T>& a, const std::complex<T>& b
+    , const std::complex<T>& c, const std::complex<T>& d)
 {
     return (a * z + b) / (c * z + d);
 }
 
 template <class T>
-std::shared_ptr<image<T>> mobius_transform(
-    const std::shared_ptr<image<T>>& input, const complex<T>& a, const complex<T>& b,
-    const complex<T>& c, const complex<T>& d, const coord2<T>& upperLeft, const coord2<T>& lowerRight,
+image<T> mobius_transform(
+    const image<T>& input, const std::complex<T>& a, const std::complex<T>& b,
+    const std::complex<T>& c, const std::complex<T>& d, const coord2<T>& upperLeft, const coord2<T>& lowerRight,
     const rgbcolor<T>& background = rgbcolor<T>(0, 0, 0)
     )
 {
     // First, calculate a few constants...
-    size_t nx = input->get_width();
-    size_t ny = input->get_height();
+    size_t nx = input.get_width();
+    size_t ny = input.get_height();
     T xstep = (lowerRight.x() - upperLeft.x()) / nx;
     T ystep = (lowerRight.y() - upperLeft.y()) / ny;
-    interval<T> xrange(0, nx);
-    interval<T> yrange(0, ny);
+    interval<T> xrange(0, T(nx));
+    interval<T> yrange(0, T(ny));
 
-    std::shared_ptr<image<T>> output(new image<T>(nx, ny));
+    image<T> output(nx, ny);
 
     for (size_t y = 0; y < ny; ++y)
     {
         for (size_t x = 0; x < nx; ++x)
         {
-            complex<T> z = complex<T>(upperLeft.x() + x * xstep
-                                      , upperLeft.y() + y * ystep);
+            std::complex<T> z = std::complex<T>(upperLeft.x() + x * xstep,
+                upperLeft.y() + y * ystep);
 
-            complex<T> foz = mobius_transform(z, a, b, c, d);
+            std::complex<T> foz = mobius_transform(z, a, b, c, d);
 
             T mx = (foz.real() - upperLeft.x()) / xstep;
             T my = (foz.imag() - upperLeft.y()) / ystep;
@@ -55,12 +51,12 @@ std::shared_ptr<image<T>> mobius_transform(
 
             if (yrange.outside(my) || xrange.outside(mx))
             {
-                (*output)(x, y) = background;
+                output(x, y) = background;
             }
             else
             {
                 // FIXME! Do color interpolation instead of block scaling (this should be ugly).
-                (*output)(x, y) = (*input)(mx, my);
+                output(x, y) = input(size_t(mx), size_t(my));
             }
         }
     }
@@ -72,28 +68,26 @@ int main(int argc, const char** argv)
 {
     typedef double float_type;
 
-    // FIXME! Customize
-    const char* const input_file = "input.tga";
-    const char* const output_file = "output.tga";
+    std::string input_file = (argc > 1) ? argv[1] : "earth.ppm";
+    std::string output_file = (argc > 2) ? argv[2] : "mobius_output.png";
 
     // If my math (in my head) works properly, this should be a 1:1 mapping of input to output.
     // I'm too tired right now (20080328) to figure out proper constants for rotation.
     // Simple scaling can be done by a = (1/scale, 0)
-    const complex<float_type> a(1, 0);
-    const complex<float_type> b(0, 0);
-    const complex<float_type> c(0, 0);
-    const complex<float_type> d(1, 0);
+    const std::complex<float_type> a(1, 0);
+    const std::complex<float_type> b(0, 0);
+    const std::complex<float_type> c(0, 0);
+    const std::complex<float_type> d(1, 0);
 
     const coord2<float_type> upperLeft(100, 100);
     const coord2<float_type> lowerRight(-100, -100);
 
     tga_io<float_type> tga;
 
-    std::shared_ptr<image<float_type>> input = tga.input(input_file);
-    std::shared_ptr<image<float_type>> output = mobius_transform(
-        input, a, b, c, d, upperLeft, lowerRight);
+    auto input = convert_image<float_type>(load_image_with_stb(input_file));
+    auto output = mobius_transform(input, a, b, c, d, upperLeft, lowerRight);
 
-    tga.output(output_file, *output);
+    save_image(output_file, output);
 
     return 0;
 }
